a19c80ae7b8505c05025b85e0e25f9f760d8c6ec
Alberto Cerrato <cerratoa2@gmail.com>
2016-11-11 13:37:15 -0600

    Update: Added input's iid to abbott04 form

--- src/components/forms/Abbott04.jsx
+++ src/components/forms/Abbott04.jsx
@@ -42,28 +42,28 @@ const Abbott04 = ({ abbott04 }) => (
             </div>
             <div className='Form-fieldSet'>
                 <DateInput className='' label='Fecha de Solicitud' date={abbott04.get('date')}/>
-                <TextInput label='Nombre del Evento' className='Form-textInputBox'/>
-                <TextInput label='Unidad de Negocio' className='Form-textInputBox'/>
-                <TextInput label='Evento Nacional/Local o Internacional:' className='Form-textInputBox'/>
+                <TextInput label='Nombre del Evento' id='nombredelevento' className='Form-textInputBox'/>
+                <TextInput label='Unidad de Negocio' id='unidaddenegocio' className='Form-textInputBox'/>
+                <TextInput label='Evento Nacional/Local o Internacional:' id='localidaddelevento' className='Form-textInputBox'/>
                 <span className='Form-label'>Racional del negocio para organizar/patrocinar el evento:</span>
-                <TextBoxInput rows='4' />
-                <TextInput label='Tipo del Evento' className='Form-textInputBox'/>
+                <TextBoxInput rows='4' id='racionaldelnegocio' />
+                <TextInput label='Tipo del Evento' id='tipodeevento' className='Form-textInputBox'/>
                 <DateInput className='' label='Fecha del Evento' date={abbott04.get('date')}/>
                 <span className='Form-label'>Producto o línea de negocio que patrocina:</span>
-                <TextInput label='Lugar' className='Form-textInputBox'/>
-                <NumberInput label='Número de asistentes si es organizado por Abbott:' className='Form-textInputBox'/>
-                <NumberInput label='Número de HCPs patrocinados si es un Congreso:' className='Form-textInputBox'/>
-                <NumberInput label='Número de Empleados de Abbott asistentes:' className='Form-textInputBox'/>
+                <TextInput label='Lugar' id='productopatrocinado' className='Form-textInputBox'/>
+                <NumberInput label='Número de asistentes si es organizado por Abbott:' id='numerodeasistentes' className='Form-textInputBox'/>
+                <NumberInput label='Número de HCPs patrocinados si es un Congreso:' id='numerodehcp' className='Form-textInputBox'/>
+                <NumberInput label='Número de Empleados de Abbott asistentes:' id='empleadosabbott' className='Form-textInputBox'/>
                 <CheckboxInput 
                     className='Checkbox-container--fiveOption'
                     label='Evento dirigido a:'
                     options={eventoTo}/>
-                <TextInput label='otro' className='Form-textInputBox'/>
+                <TextInput label='otro' id='dirigidoaotro' className='Form-textInputBox'/>
                 <NumericTable className='Form-expensesTable' headerArray={planedExpenses} fields={expensesList} />
-                <TextInput label='Nombre y Cargo del Solicitante' className='Form-textInputBox'/>
-                <TextInput label='Gerente de distrito' className='Form-textInputBox'/>
+                <TextInput label='Nombre y Cargo del Solicitante' id='nombresolicitante' className='Form-textInputBox'/>
+                <TextInput label='Gerente de distrito' id='gerentededistrito' className='Form-textInputBox'/>
                 <Notes notes={notes.districtManager} />
-                <TextInput label='Gerente de Pais' className='Form-textInputBox'/>
+                <TextInput label='Gerente de Pais' id='gerentedepais' className='Form-textInputBox'/>
                 <span className='Form-label Form-label--under'>Aprobaciones:</span>
                 <Firm label='Gerente de Producto o Unidad de Negocio:' date={todayDate} />
                 <Firm label='Gerente Medico:' date={todayDate} />
