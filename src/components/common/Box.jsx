6f7e3bc7d068e87a41c9ed55982d32d61cb04967
Douglas <Douglasacost@hotmail.com>
2017-08-22 21:04:09 -0430

    nonStyle update

--- src/components/common/Box.jsx
+++ src/components/common/Box.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class Button extends Component {
     render(){
         let {className, children, nonStyle, style} = this.props;
-        style = nonStyle ? {backgroundColor: 'white'} : style;
+        style = nonStyle ? {backgroundColor: 'white', border: 'none'} : style;
         return <div style={style} className={"Box " + className || "Box"}>{children}</div>;
     }
 }
